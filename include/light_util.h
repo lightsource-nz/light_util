74553a566ee21b22d0856cb2993c217dc288216b
tendrils <tendrils@users.noreply.github.com>
2023-01-13 21:32:28 +1300

    Initial commit

---
+++ include/light_util.h
@@ -0,0 +1,22 @@
+#ifndef _LIGHT_UTIL_H
+#define _LIGHT_UTIL_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// type manipulation macros shamelessly borrowed from the Linux kernel
+#define container_of(ptr, type, member) ({                          \
+        void *__mptr = (void *)(ptr);                               \
+        static_assert(__same_type(*(ptr), ((type *)0)->member) ||   \
+                __same_type(*(ptr), void),                          \
+                "pointer type mismatch in container_of()");         \
+        ((type *)(__mptr - offsetof(type, member))); })
+
+#ifndef __same_type
+# define __same_type(a, b) __builtin_types_compatible_p(typeof(a), typeof(b))
+#endif
+
+extern void light_util_init();
+
+#endif
---
+++ src/light_util.c
@@ -0,0 +1,18 @@
+/*
+ *  light_object.c
+ *  core definitions for the light object model
+ * 
+ *  authored by Alex Fulton
+ *  created january 2023
+ * 
+ */
+
+#include "light_util.h"
+
+#include <stdio.h>
+#include <string.h>
+
+void light_util_init()
+{
+    
+}
