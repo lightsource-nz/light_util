f4a07f1e1bde6ffda6896d4024ff53efb11c08f5
tendril <alex.fulton.nz@gmail.com>
2023-01-25 14:35:11 +1300

    revert unnecessary changes

--- include/light_util_compiler.h
+++ include/light_util_compiler.h
@@ -1,10 +1,5 @@
 #ifndef _LIGHT_UTIL_COMPILER_H
 #define _LIGHT_UTIL_COMPILER_H
-
-#ifdef _Static_assert
-#define static_assert(cond, msg) static_assert(cond, msg)
-#else
-// TODO provide an implementation of static_assert for non-C11 programs
-#endif
+// define compiler-specific hacks here
 
 #endif
